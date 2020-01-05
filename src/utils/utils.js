63fbc3c448e774526a59cbdbee598f12e7c6961e
aaron <playitsafe@qq.com>
2020-01-05 22:52:41 +1100

    manage city form done

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,10 @@
 export default {
   formateDate(date) {
     if (!date) return '';
-    
+    if (typeof date == 'number') { date = new Date(date); }
+
     return date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+
   },
 
   pagination(data, callback) {
